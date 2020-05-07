ad77ad7fa30a18103bee86315fbbce745fff97eb
Dennis Rudolph <45796605+drudolpho@users.noreply.github.com>
2020-05-07 11:37:14 -0400

    Refactored day model to store count, createDays() working properly

--- Pushup/Pushup/Core Data/Set+Convenience.swift
+++ Pushup/Pushup/Core Data/Set+Convenience.swift
@@ -10,20 +10,21 @@ import Foundation
 import CoreData
 
 extension SetOfPushups {
-    convenience init(pushups: Int, time: Int, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    convenience init(pushups: Int, time: Int, date: Date = Date(), context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.pushups = Int32(pushups)
         self.time = Int32(time)
-        self.date = Date()
+        self.date = date
     }
 }
 
 extension Day {
-    convenience init(pushups: Int, average: Int, sets: Int, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+    convenience init(pushups: Int, average: Int, sets: Int, date: Date = Date(), count: Int, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
         self.init(context: context)
         self.pushups = Int32(pushups)
         self.average = Int32(average)
-        self.date = Date()
+        self.date = date
         self.sets = Int32(sets)
+        self.count = Int32(count)
     }
 }
