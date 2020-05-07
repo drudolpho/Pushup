a6025f0519c07c7f031524bb4c8ef23a50d4fb20
Dennis Rudolph <45796605+drudolpho@users.noreply.github.com>
2020-05-07 19:58:24 -0400

    Finished mvp graph

--- Pushup/Pushup/View Controllers/DataViewController.swift
+++ Pushup/Pushup/View Controllers/DataViewController.swift
@@ -13,14 +13,16 @@ import Charts
 class DataViewController: UIViewController {
     
     var dataController: DataController?
+    var dayCache: [Double: (Date, String)] = [:]
     
     @IBOutlet weak var totalPushupsLabel: UILabel!
     @IBOutlet weak var avgPushupsLabel: UILabel!
     @IBOutlet weak var totalSetsLabel: UILabel!
     @IBOutlet weak var totalTimeLabel: UILabel!
     @IBOutlet weak var chartFrameView: UIView!
+    @IBOutlet weak var dailyAvgLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var lineChartView: LineChartView!
-    @IBOutlet weak var noDataLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,7 +48,6 @@ class DataViewController: UIViewController {
             self.dataController?.deleteData(completion: { (deletionWorked) in
                 if deletionWorked {
                     DispatchQueue.main.async {
-                        self.dataController?.fetchSetData()
                         self.updateViews()
                         self.setupChart()
                     }
@@ -77,11 +78,9 @@ class DataViewController: UIViewController {
         //Total time
         totalTimeLabel.text = dataController.getTotalTimeAsString()
         
-        if totalPushups == 0 {
-            noDataLabel.isHidden = false
-        } else {
-            noDataLabel.isHidden = true
-        }
+        let dateAsString = dataController.formatter.string(from: dataController.dayData?.last?.date ?? Date())
+        dailyAvgLabel.text = "Daily Avg: \(dataController.dayData?.last?.average ?? 0)"
+        dateLabel.text = dateAsString
     }
     
     @objc func screenEdgeSwiped(_ recognizer: UIScreenEdgePanGestureRecognizer) {
@@ -91,19 +90,74 @@ class DataViewController: UIViewController {
     }
     
     func setupChart() {
-        guard let dataController = dataController, let dayData = dataController.dayData else { return }
-        guard dayData.count > 0 else { return }
-        //Fetch Data
+        //Frame Setup
+        chartFrameView.layer.cornerRadius = 40
+        chartFrameView.layer.cornerRadius = 40
+        chartFrameView.layer.shadowColor = UIColor.lightGray.cgColor
+        chartFrameView.layer.shadowOpacity = 0.3
+        chartFrameView.layer.shadowOffset = .zero
+        chartFrameView.layer.shadowRadius = 10
+        
+        //Chart Setup
+//        lineChartView.backgroundColor = chartFrameView.backgroundColor
+        lineChartView.backgroundColor = .clear
+        lineChartView.drawGridBackgroundEnabled = false
+        lineChartView.rightAxis.enabled = false
+        lineChartView.legend.enabled = false
+        lineChartView.pinchZoomEnabled = false
+        lineChartView.doubleTapToZoomEnabled = false
+        lineChartView.delegate = self
+//        lineChartView.dragEnabled = false
+        
+        guard let dataController = dataController, let dayData = dataController.dayData, let lastDay = dayData.last else { return }
+        
+        let yAxis = lineChartView.leftAxis
+        yAxis.labelFont = UIFont(name: "SeoulNamsan", size: 12) ?? UIFont.boldSystemFont(ofSize: 10) //Make custom font
+//        yAxis.setLabelCount(6, force: false)
+        yAxis.axisMinimum = 0
+        yAxis.axisMaximum = Double(dataController.getHighestDayAvg()) * 1.5
+        yAxis.axisMinLabels = 5
+        yAxis.granularityEnabled = true
+        yAxis.granularity = 1
+        yAxis.axisLineColor = totalPushupsLabel.textColor
+        yAxis.labelTextColor = totalPushupsLabel.textColor
+        yAxis.gridColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1.0)
+        
+        lineChartView.xAxis.labelPosition = .bottom
+        lineChartView.xAxis.labelFont = UIFont(name: "SeoulNamsan", size: 12) ?? UIFont.boldSystemFont(ofSize: 10)
+        lineChartView.xAxis.axisMinimum = 1
+//        lineChartView.xAxis.axisMinLabels = 12
+        if lastDay.count > 7 {
+            lineChartView.xAxis.axisMaximum = Double(lastDay.count)
+        } else {
+            lineChartView.xAxis.axisMaximum = 7
+        }
+        lineChartView.xAxis.axisLineColor = totalPushupsLabel.textColor
+        lineChartView.xAxis.labelTextColor = totalPushupsLabel.textColor
+        lineChartView.xAxis.drawGridLinesEnabled = false
+        lineChartView.xAxis.granularityEnabled = true
+        lineChartView.xAxis.granularity = 1
+        lineChartView.xAxis.labelCount = 7
+//        let numbers = ["1", "2", "3", "4", "5", "6", "7"]
+//        lineChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values:numbers)
+//        lineChartView.xAxis.granularity = 1
+//        lineChartView.xAxis.setLabelCount(7, force: true)
+        
+        
+        //Data Setup
+        if dayData.count <= 1 {
+            guard dayData.first?.pushups ?? 0 > Int32(0) else { return }
+        }
+        
         var entries: [ChartDataEntry] = []
-        var count: Int = 0
+        
         for day in dayData {
-            count += 1
-            let entry = ChartDataEntry(x: Double(count), y: Double(day.average))
+            dayCache[Double(day.count)] = (day.date ?? Date(), String(day.average))
+            let entry = ChartDataEntry(x: Double(day.count), y: Double(day.average))
             entries.append(entry)
             print(day)
         }
         let set = LineChartDataSet(entries: entries, label: "")
-        
         if dayData.count < 2 {
             set.drawCirclesEnabled = true
             set.drawCircleHoleEnabled = false
@@ -121,51 +175,24 @@ class DataViewController: UIViewController {
         set.drawHorizontalHighlightIndicatorEnabled = false
         set.highlightColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1.0)
         let data = LineChartData(dataSet: set)
+        let format = NumberFormatter()
+        format.numberStyle = .none
+        let formatter = DefaultValueFormatter(formatter: format)
+        data.setValueFormatter(formatter)
         data.setDrawValues(false)
         lineChartView.data = data
-        
-        //Frame Setup
-        chartFrameView.layer.cornerRadius = 40
-        chartFrameView.layer.cornerRadius = 40
-        chartFrameView.layer.shadowColor = UIColor.lightGray.cgColor
-        chartFrameView.layer.shadowOpacity = 0.3
-        chartFrameView.layer.shadowOffset = .zero
-        chartFrameView.layer.shadowRadius = 10
-        
-        
-        //Chart Setup
-        //        lineChartView.backgroundColor = chartFrameView.backgroundColor
-        lineChartView.backgroundColor = .clear
-        lineChartView.drawGridBackgroundEnabled = false
-        lineChartView.rightAxis.enabled = false
-        lineChartView.legend.enabled = false
-        
-        let yAxis = lineChartView.leftAxis
-        yAxis.labelFont = .boldSystemFont(ofSize: 12) //Make custom font
-        yAxis.setLabelCount(6, force: false)
-//        yAxis.axisMinimum = 0
-//        yAxis.axisMinLabels = 5
-//        yAxis.granularityEnabled = true
-//        yAxis.granularity = 1
-        yAxis.axisLineColor = totalPushupsLabel.textColor
-        yAxis.labelTextColor = totalPushupsLabel.textColor
-        yAxis.gridColor = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1.0)
-        
-        lineChartView.xAxis.labelPosition = .bottom
-        lineChartView.xAxis.setLabelCount(7, force: false)
-//        lineChartView.xAxis.granularityEnabled = true
-//        lineChartView.xAxis.granularity = 1
-        lineChartView.xAxis.axisLineColor = totalPushupsLabel.textColor
-        lineChartView.xAxis.labelTextColor = totalPushupsLabel.textColor
-        lineChartView.xAxis.drawGridLinesEnabled = false
-        
         //        lineChartView.animate(xAxisDuration: 1)
     }
 }
     
 
-//extension DataViewController: ChartViewDelegate {
-//    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
-//        // Handle selected data
-//    }
-//}
+extension DataViewController: ChartViewDelegate {
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        let date = self.dayCache[entry.x]?.0
+        let avg = self.dayCache[entry.x]?.1
+        let dateAsString = dataController?.formatter.string(from: date ?? Date())
+        
+        dailyAvgLabel.text = "Daily Avg: \(avg ?? "")"
+        dateLabel.text = dateAsString
+    }
+}
